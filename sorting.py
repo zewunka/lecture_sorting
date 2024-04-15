d9dc05c0491bf2af1ed3f313b23331900f881d6d
Eva Víšková <eva.visk@seznam.cz>
2024-04-15 12:21:44 +0200

    message

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -8,9 +8,33 @@ def read_data(file_name):
     :param file_name: (str), name of CSV file
     :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column
     """
+    slovnik = {}
+    klice = []
+    hodnoty = []
     cwd_path = os.getcwd()
-    file_path = os.path.join(cwd_path, file_name)
+    file_path = os.path.join(cwd_path+ "/lecture11/lecture_sorting/" + file_name)
+    with open(file_path, mode="r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for header,value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
+    return data
 
+
+def selection_sort(cisla,direction = 1 ):
+    for x in range(len(cisla)):
+        for k in range(x+1,len(cisla)):
+            if direction ==1:
+                if cisla[x] > cisla[k]:
+                    cisla[x], cisla[k] = cisla[k], cisla[x]
+            else:
+                    if cisla[x] < cisla[k]:
+                        cisla[x], cisla[k] = cisla[k], cisla[x]
+    return cisla
 
 def main():
     pass
